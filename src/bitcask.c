23f717cfc48c908868a46190c86ba69c771aa2ba
perrynzhou <perrynzhou@gmail.com>
2022-05-12 02:40:21 +0000

    add art tree

---
+++ src/bitcask.c
@@ -0,0 +1,8 @@
+/*************************************************************************
+    > File Name: bitcask.c
+  > Author:perrynzhou 
+  > Mail:perrynzhou@gmail.com 
+  > Created Time: Thu 12 May 2022 02:34:25 AM UTC
+ ************************************************************************/
+
+#include <stdio.h>
---
+++ src/bitcask.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: bitcask.h
+  > Author:perrynzhou 
+  > Mail:perrynzhou@gmail.com 
+  > Created Time: Thu 12 May 2022 02:34:19 AM UTC
+ ************************************************************************/
+
+#ifndef _BITCASK_H
+#define _BITCASK_H
+typedef struct {
+
+}bitcak;
+#endif
